a1940582ae30eb7dc1aa6f1f295018d4bd3bdf48
Pasquale Gravante <p.gravante18@gmail.com>
2025-05-30 16:55:06 +0200

    Add - error enum and expose function `rle_init`,`rle_cleanup` via new interface functions `rle_execute_init`,`rle_execute_cleanup` * parameter `RLEContext* ctx` is replaced with `RLEContext** ctx` in init(s) and cleanup(s) methods to enable pointer reassignment * `get_rate` function is no more exposed since has no use outside `rle_tools.c`

--- rle_tools.c
+++ rle_tools.c
@@ -1,34 +1,139 @@
 #include "rle_tools.h"
 
-int rle_init(RLEContext *ctx, const char *input_path, const char *output_path, size_t buffer_size){
-    ctx->input=fopen(input_path, "rb");
-    ctx->output=fopen(output_path, "wb");
-    ctx->buffer_size=buffer_size;
-    ctx->buffer=(unsigned char*)malloc(buffer_size);
-    ctx->total_bytes_output=0;
-    ctx->total_bytes_processed=0;
+enum RLEResult rle_init(RLEContext** ctx, const char *input_path, const char *output_path, size_t buffer_size){
+    *ctx = (RLEContext*)malloc(sizeof(RLEContext));
 
-    return 20*(ferror(ctx->input) || ferror(ctx->output));//error code: 20, otherwise 0
+    if(*ctx==NULL){
+        return RLE_ENOMEM_CTX;
+    }
+
+    (*ctx)->input=fopen(input_path, "rb");
+    if(ferror((*ctx)->input)!=0){
+        return RLE_EOPEN_INPUT;
+    }
+
+    (*ctx)->output=fopen(output_path, "wb");
+    if(ferror((*ctx)->output)!=0){
+        return RLE_EOPEN_OUTPUT;
+    }
+
+    (*ctx)->buffer_size=buffer_size;
+    if((*ctx)->buffer_size<=0 || (*ctx)->buffer_size>RLE_DEFAULT_BUFFER_SIZE){
+        return RLE_BAD_BUFFER_SIZE;
+    }
+
+    (*ctx)->buffer=(unsigned char*)malloc(buffer_size);
+    if((*ctx)->buffer==NULL){
+        return RLE_ENOMEM_BUFFER;
+    }
+
+    (*ctx)->total_bytes_output=0;
+    (*ctx)->total_bytes_processed=0;
+
+    return RLE_OK;
 }
 
-void rle_cleanup(RLEContext *ctx){
-    fclose(ctx->output);
-    fclose(ctx->input);
-    free(ctx->buffer);
+enum RLEResult rle_cleanup(RLEContext** ctx, const _Bool deallocate_file_names){
+    if(fclose((*ctx)->output)!=0){
+        return RLE_ECLOSE_OUTPUT;
+    }
+    if(fclose((*ctx)->input)!=0){
+        return RLE_ECLOSE_INPUT;
+    }
 
-    ctx->buffer=NULL;
-    ctx->buffer_size=RLE_DEFAULT_BUFFER_SIZE;
-    ctx->input=NULL;
-    ctx->output=NULL;
-    ctx->total_bytes_output=0;
-    ctx->total_bytes_processed=0;
+    free((*ctx)->buffer); //no possible check, only error is it can be freed twice
+
+    (*ctx)->buffer=NULL;
+    (*ctx)->buffer_size=RLE_DEFAULT_BUFFER_SIZE;
+    if(deallocate_file_names!=0){
+        free((*ctx)->input);
+        free((*ctx)->output);
+        (*ctx)->input=NULL;
+        (*ctx)->output=NULL;
+    }
+    (*ctx)->total_bytes_output=0;
+    (*ctx)->total_bytes_processed=0;
+
+    free(*ctx);
+    //printf("OK dealloc of *ctx\n");
+    //free(ctx);
+    //printf("OK dealloc of ctx\n");
+    return RLE_OK;
 }
 
-float get_compression_rate(const RLEContext* const context){
-    return (float)context->total_bytes_output/context->total_bytes_processed*100;
+float get_rate(const RLEContext* const context){
+    return (float)context->total_bytes_output/context->total_bytes_processed;
 }
 
-void rle_deallocate_resources(RLEContext* context){
-    rle_cleanup(context);
-    free(context);
+enum RLEResult rle_execute_init(RLEContext** ctx, const char *input_path, const char *output_path, size_t buffer_size){
+    enum RLEResult res = rle_init(ctx, input_path, output_path, buffer_size);
+    
+    if(res==RLE_OK){
+        return res;
+    }
+
+    printf("Error[RLE Init] with id %d: ", res);
+    switch (res)
+    {
+    case RLE_ENOMEM_CTX:
+        puts("insufficient heap memory to allocate RLEContext");
+        break;
+    
+    case RLE_ENOMEM_BUFFER:
+        puts("insufficient heap memory to allocate buffer of RLEContext");
+        break;
+    
+    case RLE_EOPEN_INPUT:
+        puts("couldn't open input file in `rb` mode (read-byte)");
+        break;
+    
+    case RLE_EOPEN_OUTPUT:
+        puts("couldn't open output file in `wb` mode (write-byte)");
+        break;
+    
+    case RLE_BAD_BUFFER_SIZE:
+        printf("buffer size must be in range [1, %d]", RLE_DEFAULT_BUFFER_SIZE);
+        break;
+
+    default:
+        puts("unknown error");
+        break;
+    }
+    puts("\n");
+
+    return res;
+}
+
+enum RLEResult rle_execute_cleanup(RLEContext** context){
+    enum RLEResult res = rle_cleanup(context, 1);
+
+    if(res==RLE_OK){
+        return res;
+    }
+
+    printf("Error[RLE CleanUp] with id %d: ", res);
+    switch (res)
+    {
+    case RLE_ECLOSE_INPUT:
+        puts("couldn't close input file");
+        break;
+    
+    case RLE_ECLOSE_OUTPUT:
+        puts("couldn't close output file");
+        break;
+
+    default:
+        puts("unknown error");
+        break;
+    }
+    puts("\n");
+    return res;
+}
+
+void print_status(_Bool compression, const RLEContext* const context){
+    puts("\n----- REPORT -----");
+    printf("Bytes processed:\t%u\n", context->total_bytes_processed);
+    printf("Bytes in output:\t%u\n", context->total_bytes_output);
+    float operation_rate = (compression==0 ? 1-get_rate(context) : get_rate(context))*100;
+    printf("%s rate:\t%0.3f%%\n", (compression==0 ? "Compression" : "Decompression"), operation_rate);
 }
--- rle_tools.h
+++ rle_tools.h
@@ -4,8 +4,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-float get_compression_rate(const RLEContext* const context);
-int rle_init(RLEContext *ctx, const char *input_path, const char *output_path, size_t buffer_size);
-void rle_deallocate_resources(RLEContext* context);
+enum RLEResult{
+    RLE_OK = 0,            // success
+
+    /* allocation failures */
+    RLE_ENOMEM_CTX,        // malloc(sizeof(RLEContext)) failed
+    RLE_ENOMEM_BUFFER,     // malloc(buffer_size) failed
+
+    /* file‐open failures */
+    RLE_EOPEN_INPUT,       // fopen(input_path,  "rb") failed
+    RLE_EOPEN_OUTPUT,      // fopen(output_path, "wb") failed
+
+    /* future I/O errors */
+    RLE_EREAD,             // read/write error on input
+    RLE_EWRITE,            // read/write error on output
+
+    RLE_ECLOSE_INPUT,
+    RLE_ECLOSE_OUTPUT,
+    
+    RLE_BAD_BUFFER_SIZE,
+
+};
+
+#ifdef RLE_TOOLS_INTERNAL
+enum RLEResult rle_init(RLEContext** ctx, const char *input_path, const char *output_path, size_t buffer_size);
+enum RLEResult rle_cleanup(RLEContext** context, _Bool deallocate_file_names);
+#endif
+
+void print_status(_Bool compression, const RLEContext* const context);
+
+enum RLEResult rle_execute_init(RLEContext** ctx, const char *input_path, const char *output_path, size_t buffer_size);
+
+enum RLEResult rle_execute_cleanup(RLEContext** context);
 
 #endif
